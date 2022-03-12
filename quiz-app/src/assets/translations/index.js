5a473408e27650a069d59775916c3165fc31d5c8
mesfin30seg <ewnetab@yahoo.com>
2022-03-12 15:38:45 +0300

    Initial commit

---
+++ quiz-app/src/assets/translations/index.js
@@ -0,0 +1,18 @@
+// index.js
+import en from './en.json';
+import fr from './fr.json';
+import ko from './ko.json';
+import id from './id.json';
+import hi from './hi.json';
+
+//export const defaultLocale = 'en';
+
+const messages = {
+	en: en[0],
+	fr: fr[0],
+	ko: ko[0],
+	id: id[0],
+	hi: hi[0],
+};
+
+export default messages;
